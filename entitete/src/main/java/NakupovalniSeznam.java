9d365fd52c309dd4f936bf173cb1a25e109c36e3
simenravnik <ravnik.simen@gmail.com>
2019-11-05 19:02:38 +0100

    Create getters and setters in entity NakupovalniSeznam

--- entitete/src/main/java/NakupovalniSeznam.java
+++ entitete/src/main/java/NakupovalniSeznam.java
@@ -42,4 +42,59 @@ public class NakupovalniSeznam {
     )
     private List<Oznaka> oznake;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public Instant getUstvarjen() {
+        return ustvarjen;
+    }
+
+    public void setUstvarjen(Instant ustvarjen) {
+        this.ustvarjen = ustvarjen;
+    }
+
+    public Uporabnik getUporabnik() {
+        return uporabnik;
+    }
+
+    public void setUporabnik(Uporabnik uporabnik) {
+        this.uporabnik = uporabnik;
+    }
+
+    public List<Artikel> getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(List<Artikel> artikli) {
+        this.artikli = artikli;
+    }
+
+    public List<Oznaka> getOznake() {
+        return oznake;
+    }
+
+    public void setOznake(List<Oznaka> oznake) {
+        this.oznake = oznake;
+    }
 }
