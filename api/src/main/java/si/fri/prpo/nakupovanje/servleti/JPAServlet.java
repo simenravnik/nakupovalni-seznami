7798104c1168d7a66011a567e602fd05e39ddf36
simenravnik <ravnik.simen@gmail.com>
2019-11-06 10:07:59 +0100

    Implement simple JPAServlet.java servlet

--- api/src/main/java/si/fri/prpo/nakupovanje/servleti/JPAServlet.java
+++ api/src/main/java/si/fri/prpo/nakupovanje/servleti/JPAServlet.java
@@ -1,9 +1,36 @@
 package si.fri.prpo.nakupovanje.servleti;
 
-import javax.servlet.http.HttpServlet;
+import si.fri.prpo.nakupovanje.zrna.UporabnikiZrno;
 
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+@WebServlet("/servlet")
 public class JPAServlet extends HttpServlet {
 
-    // inject UporabnikiZrno
-    // TODO: implement methods
+    @Inject
+    private UporabnikiZrno uporabnikiZrno;
+
+    private static final Logger log = Logger.getLogger(JPAServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+
+        // printing the users
+        out.append("<br>Uporabniki:<br>");
+        uporabnikiZrno.pridobiUporabnike().stream().forEach(u -> out.append(u.toString() + "<br><br>"));
+    }
+
 }
