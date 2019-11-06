df5843b44f4f68375b658b3aa4bd0fbc7edd16cc
simenravnik <ravnik.simen@gmail.com>
2019-11-06 18:29:02 +0100

    Implement JPAServlet.java to print users in http servlet

--- api/src/main/java/si/fri/prpo/nakupovanje/servleti/JPAServlet.java
+++ api/src/main/java/si/fri/prpo/nakupovanje/servleti/JPAServlet.java
@@ -1,5 +1,6 @@
 package si.fri.prpo.nakupovanje.servleti;
 
+import si.fri.prpo.nakupovanje.entitete.Uporabnik;
 import si.fri.prpo.nakupovanje.zrna.UporabnikiZrno;
 
 import javax.inject.Inject;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.logging.Logger;
 
 @WebServlet("/servlet")
@@ -29,8 +31,15 @@ public class JPAServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         // printing the users
-        out.append("<br>Uporabniki:<br>");
-        uporabnikiZrno.pridobiUporabnike().stream().forEach(u -> out.append(u.toString() + "<br><br>"));
+        out.append("<br><h1>Uporabniki:</h1>");
+        List<Uporabnik> uporabniki = uporabnikiZrno.pridobiUporabnike();
+        uporabniki.forEach(u -> {
+            out.append("Ime: " + u.getIme() + "<br>");
+            out.append("Priimek: " + u.getPriimek() + "<br>");
+            out.append("Uporabnisko ime: " + u.getUporabniskoIme() + "<br>");
+            out.append("Email: " + u.getEmail() + "<br>");
+            out.append("<br>");
+        });
     }
 
 }
