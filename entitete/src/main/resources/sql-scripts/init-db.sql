ad04aff14c980bd1f838d4223abe794beeada7ee
simenravnik <ravnik.simen@gmail.com>
2019-11-06 09:57:09 +0100

    Add basic init-db.sql

---
+++ entitete/src/main/resources/sql-scripts/init-db.sql
@@ -0,0 +1,2 @@
+INSERT INTO uporabnik (ime, priimek, email, uporabnisko_ime, geslo) VALUES ('Petra', 'Kos', 'petra.kos@hotmail.com', 'petrakos', '12345');
+INSERT INTO uporabnik (ime, priimek, email, uporabnisko_ime, geslo) VALUES ('Miha', 'Novak', 'miha.novak@gmail.com', 'mihanovak', '1998');
