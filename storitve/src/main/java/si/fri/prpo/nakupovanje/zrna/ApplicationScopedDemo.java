310d39b248cf360bb7f885b8d24a598cd384c9c2
simenravnik <ravnik.simen@gmail.com>
2019-11-09 17:09:52 +0100

    Add difference demonstration between ApplicationScope and RequestScope

---
+++ storitve/src/main/java/si/fri/prpo/nakupovanje/zrna/ApplicationScopedDemo.java
@@ -0,0 +1,48 @@
+package si.fri.prpo.nakupovanje.zrna;
+
+import si.fri.prpo.nakupovanje.entitete.Uporabnik;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+public class ApplicationScopedDemo {
+
+
+    Logger log = Logger.getLogger(ApplicationScopedDemo.class.getName());
+
+    private UUID id;
+
+    @PersistenceContext(unitName = "nakupovalni-seznami-jpa")
+    private EntityManager em;
+
+    @PostConstruct
+    private void init() {
+
+        id = UUID.randomUUID();
+
+        log.info("Zaganjam zrno z id: " + id.toString());
+
+    }
+
+    @PreDestroy
+    private void destroy() {
+
+        log.info("Unicujem zrno z id: " + id.toString());
+
+    }
+
+    public List<Uporabnik> pridobiUporabnike() {
+
+        List<Uporabnik> uporabniki = em.createNamedQuery("Uporabnik.getAll").getResultList();
+
+        return uporabniki;
+
+    }
+}
---
+++ storitve/src/main/java/si/fri/prpo/nakupovanje/zrna/RequestScopedDemo.java
@@ -0,0 +1,47 @@
+package si.fri.prpo.nakupovanje.zrna;
+
+import si.fri.prpo.nakupovanje.entitete.Uporabnik;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@RequestScoped
+public class RequestScopedDemo {
+
+    Logger log = Logger.getLogger(RequestScopedDemo.class.getName());
+
+    private UUID id;
+
+    @PersistenceContext(unitName = "nakupovalni-seznami-jpa")
+    private EntityManager em;
+
+    @PostConstruct
+    private void init() {
+
+        id = UUID.randomUUID();
+
+        log.info("Zaganjam zrno z id: " + id.toString());
+
+    }
+
+    @PreDestroy
+    private void destroy() {
+
+        log.info("Unicujem zrno z id: " + id.toString());
+
+    }
+
+    public List<Uporabnik> pridobiUporabnike() {
+
+        List<Uporabnik> uporabniki = em.createNamedQuery("Uporabnik.getAll").getResultList();
+
+        return uporabniki;
+
+    }
+}
